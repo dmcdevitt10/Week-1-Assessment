c0fb250714e918d8b93c7ae581dfb5a9341dfbcb
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-02-17 11:36:23 -0700

    2 problems solved in file: sdlc.js

--- sdlc.js
+++ sdlc.js
@@ -25,3 +25,13 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = 'This is where stacks come into play. You need to think of all the things you need in order for your software to perform and be successful'
+
+const design = 'This is where you where you take the requirements for you software and decide what needs to happen first, their relationship with each other, and how you will go about building the software.'
+
+const implementation = 'This is where you actually start to code and build your software'
+
+const testingAndIntegration = 'As you build your software, you want to test it to make sure it will perform and fix any errors that occur. You want to do this throughout the whole building process, not just at the end.'
+
+const maintain = 'After your software is up and running, you want to check your software often to fix any bugs or errors that occur so it can stay up and running.'
